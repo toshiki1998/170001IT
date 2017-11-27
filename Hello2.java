f62546b69a96f5cca954c95ddc45c1a008dfce12
toshiki1998 <33944505+toshiki1998@users.noreply.github.com>
2017-11-27 10:09:29 +0900

    Add files via upload

---
+++ Hello2.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+   import java.awt.BorderLayout;
+   
+   public class Hello2 {
+     public static void main(String[] args) {
+       JFrame frame = new JFrame("HELLO");
+       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       frame.setBounds(200, 200, 200, 200);
+       //JButton btn = new JButton("PUSH!");
+      // frame.getContentPane().add(btn, BorderLayout.CENTER);
+       
+       
+       
+       frame.setVisible(true);
+     }
+   }
